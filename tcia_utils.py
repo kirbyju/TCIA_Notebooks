1aae3a7dd1a77fc5aabf7fc9893f256fbc9d12ab
Justin Kirby <kirbyju@mail.nih.gov>
2022-11-13 12:32:56 -0500

    ENH: added setApiUrl() and improved getToken()

--- tcia_utils.py
+++ tcia_utils.py
@@ -5,26 +5,56 @@ import json
 import getpass
 import zipfile
 from io import BytesIO
+from datetime import datetime
+from datetime import timedelta
 
-####### URLs
+####### set API base_url
+# TODO: add checks make sure there's a valid token if restricted APIs are selected and create one if there's not
 
-# Search APIs (no login required) - Documentation at https://wiki.cancerimagingarchive.net/x/fILTB
-pub_search = "https://services.cancerimagingarchive.net/nbia-api/services/v1/"
-nlst_search = "https://services.cancerimagingarchive.net/nlst-api/services/v1/"
-
-# Search w/ Authentication API - Documentation at https://wiki.cancerimagingarchive.net/x/X4ATBg
-auth_search = "https://services.cancerimagingarchive.net/nbia-api/services/v2/"
-
-# Advanced API - Documentation at https://wiki.cancerimagingarchive.net/x/YoATBg
-advanced = "https://services.cancerimagingarchive.net/nbia-api/services/"
-nlst_adv = "https://services.cancerimagingarchive.net/nlst-api/services/"
-
-# token URLs (covered in Authentication API and Advanced API docs)
-token_url = "https://services.cancerimagingarchive.net/nbia-api/oauth/token?username="
-nlst_token_url = "https://nlst.cancerimagingarchive.net/nbia-api/oauth/token?username="
+def setApiUrl(api_url = ""):
+    if api_url == "":
+        # Search API (no login required): https://wiki.cancerimagingarchive.net/x/fILTB
+        base_url = "https://services.cancerimagingarchive.net/nbia-api/services/v1/"
+        print("Using open-access APIs from", base_url)
+        return base_url
+    elif api_url == "nlst":
+        # Search API (no login required): https://wiki.cancerimagingarchive.net/x/fILTB
+        base_url = "https://services.cancerimagingarchive.net/nlst-api/services/v1/"
+        print("Using open-access NLST APIs from", base_url)
+        return base_url
+    elif api_url == "restricted":
+        # Restricted-access API (login required): https://wiki.cancerimagingarchive.net/x/X4ATBg
+        #if token_status = "valid":
+        api_call_headers = {'Authorization': 'Bearer ' + access_token}
+        base_url = "https://services.cancerimagingarchive.net/nbia-api/services/v2/"
+        print("Using restricted-access APIs from", base_url)
+        return base_url
+        #else:
+            #print("You must create a token to access this API.")
+            #access_token = getToken()
+    elif api_url == "advanced":
+        # Advanced API (login required): https://wiki.cancerimagingarchive.net/x/YoATBg
+        base_url = "https://services.cancerimagingarchive.net/nbia-api/services/"
+        print("Using Advanced APIs from", base_url)
+        return base_url
+    elif api_url == "nlst-advanced":
+        # Advanced API docs (login required): https://wiki.cancerimagingarchive.net/x/YoATBg
+        base_url = "https://services.cancerimagingarchive.net/nlst-api/services/"
+        print("Using Advanced NLST APIs from", base_url)
+        return base_url
+    else:
+        base_url = "https://services.cancerimagingarchive.net/nbia-api/services/v1/"
+        print("Invalid selection. Try 'nlst', 'restricted', 'advanced', and 'nlst-advanced' for special use cases.\nDefaulting to open-access APIs from", base_url)
+        return base_url
 
 ####### get an API token to access restricted collections
+# TODO: add feature to track token expiration time/status
+
 def getToken(api_url = ""): 
+
+    # token URLs
+    token_url = "https://services.cancerimagingarchive.net/nbia-api/oauth/token?username="
+    nlst_token_url = "https://nlst.cancerimagingarchive.net/nbia-api/oauth/token?username="
 
     # set user name and password
     print("Enter User: ")
@@ -35,16 +65,25 @@ def getToken(api_url = ""):
     try:
         if api_url == "nlst":
             # create nlst token
-            # Needed for Search w/ Authentication & Advanced APIs
             url = nlst_token_url + userName + "&password=" + passWord + "&grant_type=password&client_id=nbiaRestAPIClient&client_secret=ItsBetweenUAndMe"
             access_token = requests.get(url).json()["access_token"]
-            print ('NLST API Token created successfully: ', access_token)
+            # track expiration status/time (2 hours from creation)
+            #current_time = datetime.now()
+            #exp_time = current_time + timedelta(hours=2)
+            #token_status = "valid"
+            print ('API Token created successfully: ', access_token)
+            #print('Token expires at', exp_time)
             return access_token
         else:
             # create regular token
             url = token_url + userName + "&password=" + passWord + "&grant_type=password&client_id=nbiaRestAPIClient&client_secret=ItsBetweenUAndMe"
             access_token = requests.get(url).json()["access_token"]
+            # track expiration status/time (2 hours from creation)
+            #current_time = datetime.now()
+            #exp_time = current_time + timedelta(hours=2)
+            #token_status = "valid"
             print ('API Token created successfully: ', access_token)
+            #print('Token expires at', exp_time)
             return access_token
 
     except requests.exceptions.HTTPError as errh:
